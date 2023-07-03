168f98539c08152baf3cf42ca88d9d8f90f5f32d
Soumik Datta <mailmeiamsa@gmail.com>
2023-07-03 12:29:40 +0600

    [wip] seller list

--- app/Http/Livewire/Admin/SellerList.php
+++ app/Http/Livewire/Admin/SellerList.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class SellerList extends Component
 {
+    protected $listeners = [
+        'openUserDeleteModal' => 'openUserDeleteModal',
+        'refreshComponent' => 'refreshChildren'
+    ];
+
+    public function refreshChildren()
+    {
+        $this->emit('refreshChildren');
+    }
+
+    public function openUserDeleteModal(User $user)
+    {
+        $this->emit('userDeleteModal', $user);
+    }
+
     public function render()
     {
         return view('livewire.admin.seller-list');
--- resources/views/livewire/admin/seller-list.blade.php
+++ resources/views/livewire/admin/seller-list.blade.php
@@ -1,5 +1,22 @@
 <div>
+    <div>
+        <div class="container-xl">
+            <!-- Page title -->
+            <div class="page-header d-print-none">
+                <h2 class="page-title">
+                    {{ __('Sellers') }}
+                </h2>
+            </div>
+        </div>
 
+        <div class="page-body px-3">
+            <div class="card">
+                <div class="card-body">
+                    <livewire:seller-table />
+                </div>
+            </div>
+        </div>
 
-
+        <livewire:admin.user-delete-modal />
+    </div>
 </div>
