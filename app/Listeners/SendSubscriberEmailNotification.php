b3c9a7ecbf0e78b1fb0a264ec1f3859503bfd5e3
Soumik Datta <mailmeiamsa@gmail.com>
2023-07-11 12:57:37 +0600

    [feat] subscription mail

--- app/Listeners/SendSubscriberEmailNotification.php
+++ app/Listeners/SendSubscriberEmailNotification.php
@@ -5,7 +5,6 @@ namespace App\Listeners;
 use App\Models\User;
 use App\Events\ProductUpdated;
 use App\Models\ProductSubscription;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -27,7 +26,7 @@ class SendSubscriberEmailNotification
     public function handle(ProductUpdated $event): void
     {
         $subscriberIds = ProductSubscription::where('product_id', '=', $event->product->id)->pluck('user_id');
-        $users = User::whereIn('user_id', $subscriberIds)->get();
+        $users = User::whereIn('id', $subscriberIds)->get();
 
         foreach ($users as $recipient) {
             Mail::to($recipient)->send(new ProductUpdatedNotificationMail($recipient, $event->product));
